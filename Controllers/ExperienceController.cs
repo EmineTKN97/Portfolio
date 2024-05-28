42fc0c6821097d1ed4e6be39b948470daa47045d
Emine Tekin <eminetekin97@outlook.com>
2024-05-28 18:31:14 +0300

    +add experiencelist +add experiencecreate

--- Controllers/ExperienceController.cs
+++ Controllers/ExperienceController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.DAL.Context;
+using Portfolio.DAL.Entities;
 
 namespace Portfolio.Controllers
 {
     public class ExperienceController : Controller
     {
-        public IActionResult Index()
+        MyPortfolioContext _context = new MyPortfolioContext();
+        public IActionResult ExperienceList()
         {
-            return View();
+            var experience = _context.Experiences.ToList();
+            return View(experience);
         }
-    }
+        [HttpGet]
+		public IActionResult AddExperience()
+        { 
+			return View();
+		}
+		[HttpPost]
+		public IActionResult AddExperience(Experience experience)
+		{
+            _context.Experiences.Add(experience);
+            _context.SaveChanges();
+			return RedirectToAction("ExperienceList");   
+		}
+	}
 }
