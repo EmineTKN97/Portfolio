215d65ff6099cb5469e8e1db3c626def7ca6e564
Emine Tekin <eminetekin97@outlook.com>
2024-05-28 14:58:28 +0300

    +add admin panel +add admin panel layout

---
+++ ViewComponents/LayoutViewComponents/_LayaoutScriptComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.ViewComponents.LayoutViewComponents
+{
+	public class _LayaoutScriptComponentPartial: ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			{
+				return View();
+			}
+		}
+	}
+}
---
+++ ViewComponents/LayoutViewComponents/_LayaoutSlideBarComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Portfolio.ViewComponents.LayoutViewComponents
+{
+	public class _LayaoutSlideBarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+		return View();
+		}
+	}
+}
